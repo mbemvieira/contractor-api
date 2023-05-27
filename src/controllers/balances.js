fad64898d4a86a0a8bb9c313d7c276b6911e59c7
Mateus <mbemvieira@gmail.com>
2023-05-27 20:20:53 +0100

    balance route

--- src/controllers/balances.js
+++ src/controllers/balances.js
@@ -1,4 +1,41 @@
-const addFunds = async (req, res) => res.json(req.params);
+const { PROFILE_TYPE_CLIENT } = require('../repositories/constants');
+const { getTotalDebit } = require('../repositories/profile');
+
+const addFunds = async (req, res) => {
+  const sequelize = req.app.get('sequelize');
+  const { Profile } = req.app.get('models');
+  const { userId } = req.params;
+  let { value } = req.body;
+
+  value = parseFloat(value);
+
+  if (Number.isNaN(value)) {
+    return res.status(422).end();
+  }
+
+  const profile = await Profile.findOne({
+    where: {
+      id: userId,
+      type: PROFILE_TYPE_CLIENT,
+    },
+  });
+
+  if (!profile) {
+    return res.status(404).end();
+  }
+
+  const totalDebit = await getTotalDebit(sequelize, userId);
+
+  if (value > (totalDebit / 4)) {
+    return res.status(422).end();
+  }
+
+  profile.balance += value;
+
+  profile.save();
+
+  return res.json(profile);
+};
 
 module.exports = {
   addFunds,
--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const { getProfile } = require('../middlewares/getProfile');
-const {
-  getBestProfession,
-  getBestClients,
-} = require('../controllers/admin');
+const { getBestProfession, getBestClients } = require('../controllers/admin');
 
 const router = express.Router();
 
